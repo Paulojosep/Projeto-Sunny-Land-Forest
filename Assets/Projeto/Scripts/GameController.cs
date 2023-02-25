20f591026ccdf359b63118b28bf1eb3de4fce649
Paulo Modesto <paulo.jmodesto@hotmail.com>
2023-02-25 16:48:41 -0300

    Adicionando Audio Da Coleta dos Itens

--- Assets/Projeto/Scripts/GameController.cs
+++ Assets/Projeto/Scripts/GameController.cs
@@ -7,10 +7,16 @@ public class GameController : MonoBehaviour
 {
     private int score;
     public Text txtScore;
+
+    public AudioSource fxGame;
+    public AudioClip fxCenouraColetada;
     
     public void Pontuacao(int qtdPontos)
     {
         score += qtdPontos;
         txtScore.text = score.ToString();
+
+        // Som da coleta da cenoura
+        fxGame.PlayOneShot(fxCenouraColetada);
     }
 }
